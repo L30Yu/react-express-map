d901007fb9ce0c0bc9a9ee593b288ee0545bf347
leo <leo@leos-MacBook-Pro.local>
2018-05-11 08:23:07 -0600

    simplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import MainLayout from './components/MainLayout';
-import AutoCompleteInput from './components/AutoCompleteInput';
+import SearchInput from './components/SearchInput';
 import { Button } from 'antd';
-import { connect } from "react-redux";
 
 class App extends Component {
-  render() {
 
-    const { fetching, addresses, onRequestAddresses, error } = this.props;
+  state = { 
+    fetching: false, 
+    addresses: null,  
+    error: null 
+  }
 
+  render() {   
+    let {fetching, addresses, error} = this.state;
     return (
       <div className="App">
         <MainLayout>
-          <AutoCompleteInput {...this.props}/>
+          <SearchInput />
           <span style={{ paddingLeft: "8px" }}>
             {fetching
               ? <Button disabled type="primary" shape="circle" icon="search" />
-              : <Button type="primary" shape="circle" icon="search" onClick={onRequestAddresses}/>
+              : <Button type="primary" shape="circle" icon="search" />
             }
           </span>
           {addresses
-            // ? <img src={addresses} />
             ? addresses
             : null
           }
@@ -30,20 +33,4 @@ class App extends Component {
   }
 }
 
-
-const mapStateToProps = state => {
-  return {
-    fetching: state.fetching,
-    addresses: state.addresses,
-    error: state.error
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onRequestAddresses: () => dispatch({ type: "API_CALL_REQUEST" }),
-    onHandleChange: (query)=> dispatch({ type: "SET_QUERY", query }),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,33 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'antd/dist/antd.css';
 
-import { createStore, applyMiddleware, compose } from "redux";
-import createSagaMiddleware from "redux-saga";
-import { Provider } from "react-redux";
-
-import { reducer } from "./redux/redux";
-import { watcherSaga } from "./saga/saga";
-
-// create the saga middleware
-const sagaMiddleware = createSagaMiddleware();
-
-// dev tools middleware
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-// create a redux store with our reducer above and middleware
-let store = createStore(
-  reducer,
-  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
-);
-
-// run the saga
-sagaMiddleware.run(watcherSaga);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById("root")
 );
 registerServiceWorker();
